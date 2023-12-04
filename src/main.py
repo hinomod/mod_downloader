36bce8caacbdebd7e4c0462454b7195aecc5e682
siyukatu <120154870+siyukatu@users.noreply.github.com>
2023-12-04 23:38:31 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,15 +6,18 @@ import tempfile
 import os
 import shutil
 import datetime
+import pathlib
 
 dotmc = os.getenv('APPDATA').replace("\\","/")+"/.minecraft"
-if not os.path.exists(dotmc):
-    os.mkdir(dotmc)
 now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
 
 import eel
 
 eel.init("web")
+
+@eel.expose
+def get_mods_path():
+    return dotmc+"/mods"
 
 @eel.expose
 def get_mods():
@@ -32,8 +35,16 @@ def get_mods():
     return {"success":True,"mods":mods}
 
 @eel.expose
-def download_mod(mod):
-    res = requests.get("https://api.github.com/repos/hinomod/mod_files/contents/mods/"+mod)
+def download_mod(mod,mods_dir=None):
+    mods_dir = mods_dir.replace("\\","/")
+    if mods_dir == None:
+        mods_dir = dotmc+"/mods"
+    else:
+        mods_dir = str(pathlib.Path(mods_dir+"/")).replace("\\","/")
+    print(mods_dir)
+    for retry in range(5):
+        try:res = requests.get("https://api.github.com/repos/hinomod/mod_files/contents/mods/"+mod);break
+        except:time.sleep(1)
     files = res.json()
     if res.status_code != 200:
         message = "ステータスコード: "+str(res.status_code)
@@ -52,7 +63,9 @@ def download_mod(mod):
     for file in files:
         if file["type"] == "file":
             history += "\nダウンロード中: "+file["name"]
-            res = requests.get(file["download_url"], stream=True)
+            for retry in range(5):
+                try:res = requests.get(file["download_url"], stream=True);break
+                except:time.sleep(1)
             if res.status_code != 200:
                 message = "ステータスコード: "+str(res.status_code)
                 if "message" in files:
@@ -65,13 +78,15 @@ def download_mod(mod):
                     eel.update_downloading_progress({"downloading":True, "progress":downloaded/total*100, "total":total, "history":history})
             history += " (完了)"
     eel.update_downloading_progress({"downloading":True, "progress":100, "total":total, "history":history})
-    try:shutil.move(dotmc+"/mods", dotmc+"/mods_"+now)
+    try:shutil.move(mods_dir, mods_dir+"_"+now)
     except:pass
-    try:shutil.move(temp_dir+"/mods", dotmc+"/mods")
-    except:pass
+    try:os.makedirs(mods_dir, exist_ok=True);shutil.rmtree(mods_dir)
+    except:eel.update_downloading_progress({"downloading":False, "is_done":True, "success":False});return
+    try:shutil.move(temp_dir+"/mods", mods_dir)
+    except:eel.update_downloading_progress({"downloading":False, "is_done":True, "success":False});return
     try:shutil.rmtree(tmp_dir)
     except:pass
-    eel.update_downloading_progress({"downloading":False, "is_done":True})
+    eel.update_downloading_progress({"downloading":False, "is_done":True, "success":True})
     return {"success":True}
 
-eel.start("main.html", size=(450, 550), port=59184)
+eel.start("main.html", size=(450, 580), port=59184)
